42bf6e443ec41834d375f2ece7257140372d0bbd
Caisin <kaixing622@qq.com>
2019-07-15 02:00:34 +0800

    修改

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+# 生成静态文件
+sudo npm run build
+
+# 进入生成的文件夹
+cd docs
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+sudo git push -f https://github.com/Caisin/caisin.github.io.git master
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
